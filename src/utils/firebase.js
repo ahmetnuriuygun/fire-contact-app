a734fc8adbe3f84a84bc5e69cf0634b6a0b656cc
ahmetnuygun <ahmetn.uygun@gmail.com>
2022-08-09 16:49:38 +0200

    The project has completed

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
+    apiKey: "AIzaSyAHzPRW18qzDrXlXejL6HnyQIeMdDfFs2Y",
+    authDomain: "fire-contact-de.firebaseapp.com",
+    databaseURL: "https://fire-contact-de-default-rtdb.firebaseio.com",
+    projectId: "fire-contact-de",
+    storageBucket: "fire-contact-de.appspot.com",
+    messagingSenderId: "388112293077",
+    appId: "1:388112293077:web:f5e2a2ab0aee484f77d65c"
 };
 
 // Initialize Firebase
